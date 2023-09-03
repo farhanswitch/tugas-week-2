63eebd74546442d8d58b11afa2c48559c7a5746c
farhanswitch <muhammad_farhan@outlook.co.id>
2023-09-03 22:35:58 +0700

    Add comments at file konversi.c

--- konversi.c
+++ konversi.c
@@ -16,7 +16,7 @@ int main()
     // Proses konversi
     // 1 jam = 3600 detik
     jam = totalDetik / 3600;
-    // 1 menit = 60 detik. Dan setiap 60 menit = 1 jam.
+    // 1 menit = 60 detik. Dan setiap 60 menit = 1 jam. Untuk mendapatkan jumlah menit selain yang sudah dikonversi menjadi jam maka kita harus melakukan operasi jumlah menit modulo 60, karena setiap 60 menit akan dikonversi menjadi jam
     menit = (totalDetik / 60) % 60;
     // 1 menit = 60 detik. Jadi untuk hitung sisa detik, total detik modulo 60
     detik = totalDetik % 60;
